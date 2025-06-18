e7fe7cb3229f9227b4cbc9f63e13ed7411e40e72
KocchiMatsuno <kocchimatsuno@gmail.com>
2025-06-18 16:09:37 +0800

    rewrite

--- ToDoList/src/usecases/GetAllTasksUseCase.ts
+++ ToDoList/src/usecases/GetAllTasksUseCase.ts
@@ -1,14 +1,14 @@
 import type { Task } from '../domain/entities/Task';
 import type { TaskRepository } from '../domain/repo/TaskRepository';
 
-export class AddTaskUseCase {
+export class GetAllTasksUseCase {
   private repo: TaskRepository;
 
   constructor(repo: TaskRepository) {
     this.repo = repo;
   }
 
-  execute(task: Task): Promise<void> {
-    return this.repo.add(task);
+  execute(): Promise<Task[]> {
+    return this.repo.getAll();
   }
 }
--- ToDoList/src/usecases/GetTaskUseCase.ts
+++ ToDoList/src/usecases/GetTaskUseCase.ts
@@ -1,14 +1,14 @@
 import type { Task } from '../domain/entities/Task';
 import type { TaskRepository } from '../domain/repo/TaskRepository';
 
-export class AddTaskUseCase {
+export class GetTaskUseCase {
   private repo: TaskRepository;
 
   constructor(repo: TaskRepository) {
     this.repo = repo;
   }
 
-  execute(task: Task): Promise<void> {
-    return this.repo.add(task);
+  execute(id: string): Promise<Task | undefined> {
+    return this.repo.get(id);
   }
 }
